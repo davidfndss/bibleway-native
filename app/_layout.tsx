beeee79100d91573190595ef02e017d3bf4c2c78
davidfndss <davidfndssdev@gmail.com>
2025-07-27 13:55:18 -0300

    Style: Splash icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,20 @@
 import { Slot } from 'expo-router';
 import '../global.css';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import * as SystemUI from 'expo-system-ui';
+import { useEffect } from 'react';
 
 export default function Layout() {
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync('#020808');
+  }, []);
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: '#020808' }}>
+      <StatusBar style="light" backgroundColor="#020808" />
+
       <Slot />
     </GestureHandlerRootView>
-  )
+  );
 }
